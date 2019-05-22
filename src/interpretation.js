1e7f9740bc1f10ccc47775504938bbdd13e994ec
Jennifer L Schmidt <jenny@bikibird.com>
2019-05-22 10:48:37 -0500

    works, but need repetition for non terminals

--- src/interpretation.js
+++ src/interpretation.js
@@ -1,4 +1,52 @@
 //TO DO: composer function
+ISHML.interpret=function(text, lexicon, grammar)
+{    
+	var tokenizer = lexicon.tokenize(text)
+	var sequence = tokenizer.next()
+	var interpretations=[]
+	var badInterpretations=[]
+	var goodInterpretations=[]
+	while (!sequence.done)
+	{
+		interpretations.push(new ISHML.Interpretation([],sequence.value.tokens))
+		//var result=grammar.parse([{gist:{},remainder:sequence.value.tokens}])
+		var result=grammar.parse(sequence.value.tokens)
+		if (result)
+		{
+			interpretations=interpretations.concat(result)
+		}
+		sequence = tokenizer.next()
+	}
+
+	interpretations.sort(function(first,second){return first.remainder.length - second.remainder.length})
+
+	var success=false
+	interpretations.some((interpretation)=>
+	{
+		if (interpretation.remainder.length>0)
+		{
+			if (success===true){return true}
+			else
+			{
+				badInterpretations.push(interpretation)
+			}	
+		}
+		else
+		{
+			goodInterpretations.push(interpretation)
+			success=true
+			return false
+		}
+	})
+	if (goodInterpretations.length>0)
+	{	
+		return goodInterpretations
+	}
+	else
+	{
+		return badInterpretations
+	}
+}
 ISHML.Interpretation=function Interpretation(gist=[],remainder=[])
 {
 	if (this instanceof ISHML.Interpretation)
