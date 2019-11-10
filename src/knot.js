8e10f255fd43f5b43c19064912330ecffc1960ba
Jennifer L Schmidt <jenny@bikibird.com>
2019-11-10 10:09:39 -0600

    converted knot to class

--- src/knot.js
+++ src/knot.js
@@ -1,32 +1,218 @@
-ishml.Knot= function Knot(id,value)
+/*
+A knot has
+non-enumerable properties
+id
+uid
+
+enumerable cords
+	each cord has a ply:
+
+user defined enumerable properties.
+*/
+ishml.Knot= class IshmlKnot
 {
-	if (this instanceof ishml.Knot)
+	constructor(id,value)
 	{
-		if (id instanceof ishml.Knot)
+		Object.assign(this,value)
+		Object.defineProperty(this, "id", {value:ishml.util.formatId(id),writable: true})
+		Object.defineProperty(this, "plies", {value:new WeakMap(),writable: true})
+		Object.defineProperty(this, "uid", {value:ishml.util.formatId(),writable: true})
+		var ply={id:undefined,weight:1,knot:this,cord:undefined,from:undefined,to:undefined,converse:undefined, hop:0}
+		var proxiedKnot= new Proxy(this, ishml.Knot.prototype.handler)
+		this.plies.set(proxiedKnot,ply)
+		this.knots.set(this.uid,this)  //DEFECT: Needed?
+		return proxiedKnot
+	}
+	get alias()
+	{
+		var ply={id:undefined,weight:1,knot:this.ply.knot,cord:undefined,from:undefined,to:undefined,converse:undefined, hop:0}
+		var proxiedKnot= new Proxy(this.ply.knot, ishml.Knot.prototype.handler)
+		this.ply.knot.plies.set(proxiedKnot,ply)
+		return proxiedKnot
+	}
+	configure(knot)
+	{
+		var id=this.id
+		Object.assign(this,knot)
+		this.id=id
+
+	}
+	cordage()
+	{
+		return [this.cord.id, this.ply.id, (this.converse && this.converse.cord.id), (this.converse && this.converse.ply.id)]
+	}
+	forget(aTerm,aDefinition)
+	{
+		var definition=Object.assign({kind:"knot",id:this.id},aDefinition)
+		this.yarn.lexicon.unregister(aTerm,definition)
+		return this
+	}
+	has(aId)
+	{
+		if (this.hasOwnProperty(ishml.util.formatId(aId)))
 		{
-			var knot=id.ply.knot
-			
+			return true
+		}
+		else {return false}
+	}
+	path(destintation,{filter=(knot)=>true,minimum=1,maximum=Infinity,via,cost}={})
+	{
+		if (via)
+		{
+			var way=new Set(Object.values(via))
+			var anyway=false
 		}
 		else
 		{
-			var knot=this
-			Object.assign(knot,value)
-			Object.defineProperty(knot, "id", {value:ishml.util.formatId(id),writable: true})
-			Object.defineProperty(knot, "plies", {value:new WeakMap(),writable: true})
-			
+			var anyway=true
+		}	
+		this.retreat=undefined
+		var queue=[this]
+		var visited = new Set()
+		var path= []
+		while (queue.length>0)
+		{
+			var knot=queue.shift()
+			console.log(knot)
+			if (knot.ply.hop<=maximum)
+			{
+				if (knot.ply.knot===destintation.ply.knot && knot.ply.hop>=minimum)
+				{
+					//found!
+					
+					var advanceKnot
+					while (knot)
+					{
+						knot.ply.advance=advanceKnot
+						path.unshift(knot)
+						advanceKnot=knot
+						knot=knot.ply.retreat
+					}	
+					return {success:true,start:path[0],end:path[path.length-1],path:path}
+				}
+				else
+				{
+					if (!visited.has(knot.ply.knot) && filter(knot))
+					{
+						visited.add(knot.ply.knot)
+						Object.values(knot).forEach(cord => 
+						{
+							if (cord instanceof ishml.Cord && (anyway || way.has(cord.id)) )
+							{
+								Object.values(cord).forEach((child)=>
+								{
+									if (!visited.has(child.ply.knot))
+									{
+										var newKnot=child.alias
+										newKnot.ply.retreat=knot
+										newKnot.ply.hop=knot.ply.hop+1
+										queue.push(newKnot)
+									}
+								})
+							}	
+						})
+					}
+				}
+			}		
+		}
+		return {success:false,start:false,end:false,path:[]}  //not found
+	}
+	retie(cordage)
+	{
+		//$.place.kitchen.contains.knife.retie("in<contains").to($.place.foyer)
+		this.untie()
+		return this.tie(cordage)
+	}
+	tie(cordage)
+	{
+		//$.place.kitchen.tie("exit:north<entrance:south").to($.place.foyer)
+		//$.person.Lizzy.tie("friendship<friendship").to($.person.Charlotte)
+
+		var fromKnot=this
+
+		var [forward,backward]=cordage.split(/[<>]/)
+		var [cordId,plyId]=forward.split(":").map(id=>ishml.util.formatId(id.trim()))
+		if(!fromKnot.hasOwnProperty(cordId)){fromKnot[cordId]=new ishml.Cord(cordId)}
+		if (backward)
+		{
+			var [converseCordId,conversePlyId]=backward.split(":").map(id=>ishml.util.formatId(id.trim()))	
 		}
 		
-		var ply={id:undefined,weight:()=>1,knot:knot,cord:undefined,from:undefined,to:undefined,converse:undefined, hop:0}
-		var proxiedKnot= new Proxy(knot, ishml.Knot.prototype.handler)
-		knot.plies.set(proxiedKnot,ply)
-		return proxiedKnot
-	}
-	else
-	{
-		return new Knot(id,value)
-	}	
-}
+		var to = (knot)=>
+		{
+			if (knot instanceof ishml.Knot)
+			{
+				var toKnot=knot
+			}
+			else
+			{
+				var toKnot=new ishml.Knot(knot)
+			}
+			if (!cordId){cordId=ishml.util.formatId()}
+			if (!plyId){plyId=toKnot.id||ishml.util.formatId(toKnot.name)}
+			
+			if (fromKnot.hasOwnProperty(cordId))
+			{
+				var cord=fromKnot[cordId]
+				
+			}	
+			else
+			{
+				var cord = new ishml.Cord(cordId)
+				fromKnot[cordId]=cord
+			}
+			var aliasToKnot=toKnot.alias
+			aliasToKnot.ply.id=plyId
+			aliasToKnot.ply.cord=cord
+			if(backward)
+			{
+				if (toKnot.hasOwnProperty(converseCordId))
+				{
+					var converseCord=toKnot[converseCordId]
+				}
+				else
+				{
+					var converseCord=new ishml.Cord(converseCordId)
+					toKnot[converseCordId]=converseCord
+					
+				}
+				if (!conversePlyId){conversePlyId=fromKnot.id||ishml.util.formatId(fromKnot.name)}
+				var aliasFromKnot=fromKnot.alias
+				aliasFromKnot.ply.id=conversePlyId
+				aliasFromKnot.ply.cord=converseCord
+				//aliasFromKnot.ply.from=toKnot
+				aliasFromKnot.ply.converse=aliasToKnot
 
+				aliasToKnot.ply.converse=aliasFromKnot
+				
+				toKnot[converseCordId][conversePlyId]=aliasFromKnot	
+			}
+
+			fromKnot[cordId][plyId]=aliasToKnot
+			return fromKnot
+		}
+		return {to:to, tie:fromKnot.tie.bind(fromKnot)}
+	}
+	untie()
+	{
+/*Knot must have been reached by traveling along a tie.
+$.room.kitchen.untie() removes the room tie for kitchen and returns kitchen.knot.
+$.room.kitchen.exit.north removes the exit north tie to foyer and returns north.knot, the bare foyer knot.
+
+$.room.kitchen.exit.north.untie()
+*/
+		var cord=this.ply.cord
+		delete cord[this.ply.id]
+		var converse=this.ply.converse
+		if (this.converse)
+		{
+			delete converse.ply.cord[converse.ply.id]
+		}
+		return this
+	}
+}
+//DEFECT: Needed?
+Object.defineProperty(ishml.knot.prototype, "knots", {value:new WeakMap(),writable: true})
 ishml.Knot.prototype.handler=
 {
 	get: function(target, property, receiver) 
@@ -36,15 +222,15 @@ ishml.Knot.prototype.handler=
 			var plies=Reflect.get(target,"plies")
 			return (plies.get(receiver))
 		}
-		if (property==="from")
+		if (property==="retreat")
 		{	
 			var plies=Reflect.get(target,"plies")
-			return (plies.get(receiver))["from"]
+			return (plies.get(receiver))["retreat"]
 		}
-		if (property==="to")
+		if (property==="advance")
 		{	
 			var plies=Reflect.get(target,"plies")
-			return (plies.get(receiver))["to"]
+			return (plies.get(receiver))["advance"]
 		}
 		if (property==="hop")
 		{	
@@ -79,16 +265,16 @@ ishml.Knot.prototype.handler=
 			plies.get(receiver)=value
 			return true
 		}
-		if (property==="from")
+		if (property==="retreat")
 		{	
 			var plies=Reflect.get(target,"plies")
-			plies.get(receiver)["from"]=value
+			plies.get(receiver)["retreat"]=value
 			return true
 		}
-		if (property==="to")
+		if (property==="advance")
 		{	
 			var plies=Reflect.get(target,"plies")
-			plies.get(receiver)["to"]=value
+			plies.get(receiver)["advance"]=value
 			return true
 		}
 		if (property==="hop")
@@ -122,202 +308,18 @@ ishml.Knot.prototype.handler=
 	}	
 }
 
-ishml.Knot.prototype.configure=function(knot)
-{
-	var id=this.id
-	Object.assign(this,knot)
-	this.id=id
-
-}	
-ishml.Knot.prototype.forget=function(aTerm,aDefinition)
-{
-	var definition=Object.assign({kind:"knot",id:this.id},aDefinition)
-	this.yarn.lexicon.unregister(aTerm,definition)
-	return this
-}
-
-ishml.Knot.prototype.has=function(aId)
-{
-	if (this.hasOwnProperty(ishml.util.formatId(aId)))
-	{
-		return true
-	}
-	else {return false}
-}
 	
-ishml.Knot.prototype.tie = function(cordage)
-{
-	var fromKnot=this
 
-	var [forward,backward]=cordage.split(/[<>]/)
-	var [cordId,plyId]=forward.split(":").map(id=>ishml.util.formatId(id.trim()))
-	if(!fromKnot.hasOwnProperty(cordId)){fromKnot[cordId]=new ishml.Cord(cordId)}
-	if (backward)
-	{
-		var [converseCordId,conversePlyId]=backward.split(":").map(id=>ishml.util.formatId(id.trim()))	
-	}
+
+
 	
-	var to = (knot)=>
-	{
-		if (knot instanceof ishml.Knot)
-		{
-			var toKnot=knot
-		}
-		else
-		{
-			var toKnot=new ishml.Knot(knot)
-		}
-		if (!cordId){cordId=ishml.util.formatId()}
-		if (!plyId){plyId=toKnot.id||ishml.util.formatId(toKnot.name)}
-		
-		if (fromKnot.hasOwnProperty(cordId))
-		{
-			var cord=fromKnot[cordId]
-			
-		}	
-		else
-		{
-			var cord = new ishml.Cord(cordId)
-			fromKnot[cordId]=cord
-		}
-		var aliasToKnot=new ishml.Knot(toKnot)
-		aliasToKnot.ply.id=plyId
-		aliasToKnot.ply.cord=cord
-		//aliasToKnot.ply.from=fromKnot
-		if(backward)
-		{
-			if (toKnot.hasOwnProperty(converseCordId))
-			{
-				var converseCord=toKnot[converseCordId]
-			}
-			else
-			{
-				var converseCord=new ishml.Cord(converseCordId)
-				toKnot[converseCordId]=converseCord
-				
-			}
-			if (!conversePlyId){conversePlyId=fromKnot.id||ishml.util.formatId(fromKnot.name)}
-			var aliasFromKnot=new ishml.Knot(fromKnot)
-			aliasFromKnot.ply.id=conversePlyId
-			aliasFromKnot.ply.cord=converseCord
-			//aliasFromKnot.ply.from=toKnot
-			aliasFromKnot.ply.converse=aliasToKnot
 
-			aliasToKnot.ply.converse=aliasFromKnot
-			
-			toKnot[converseCordId][conversePlyId]=aliasFromKnot	
-		}
 
-		fromKnot[cordId][plyId]=aliasToKnot
-		return fromKnot
-	}
-	return {to:to, tie:fromKnot.tie.bind(fromKnot)}
-}
-ishml.Knot.prototype.track=function(journal)
-{
-	if (journal instanceof Set)
-	{
-		journal.add(this)
-	}
-	else
-	{
-		journal.push(this)
-	}
-	return this
-}
 
-ishml.Knot.prototype.cordage=function()
-{
-	return [this.cord.id, this.ply.id, (this.converse && this.converse.cord.id), (this.converse && this.converse.ply.id)]
-}
 
-ishml.Knot.prototype.untie = function()
-{
-/*Knot must have been reached by traveling along a tie.
-$.room.kitchen.untie() removes the room tie for kitchen and returns kitchen.knot.
-$.room.kitchen.exit.north removes the exit north tie to foyer and returns north.knot, the bare foyer knot.
 
-$.room.kitchen.exit.north.untie()
-*/
-	var cord=this.ply.cord
-	delete cord[this.ply.id]
-	var converse=this.ply.converse
-	if (this.converse)
-	{
-		delete converse.ply.cord[converse.ply.id]
-	}
-	return this
-}
-ishml.Knot.prototype.retie = function(cordage)
-{
-	//$.place.kitchen.contains.knife.retie("in<contains").to($.place.foyer)
-	this.untie()
-	return this.tie(cordage)
-}
-ishml.Knot.prototype.path=function(destintation,{filter=(knot)=>true,minimum=1,maximum=Infinity,via,cost}={})
-{
-	if (via)
-	{
-		var way=new Set(Object.values(via))
-		var anyway=false
-	}
-	{
-		var anyway=true
-	}	
-	this.from=undefined
-	var queue=[this]
-	var visited = new Set()
-	var path= []
-	while (queue.length>0)
-	{
-		var knot=queue.shift()
-		if (knot.ply.hop<=maximum)
-		{
-			if (knot.ply.knot===destintation.ply.knot && knot.ply.hop>=minimum)
-			{
-				//found!
-				
-				var toKnot
-				while (knot)
-				{
-					knot.ply.to=toKnot
-					path.unshift(knot)
-					toKnot=knot
-					knot=knot.ply.from
-				}	
-				return {success:true,start:path[0],end:path[path.length-1],path:path}
-			}
-			else
-			{
-				if (!visited.has(knot.ply.knot) && filter(knot))
-				{
-					visited.add(knot.ply.knot)
-					Object.values(knot).forEach(cord => 
-					{
-						if (cord instanceof ishml.Cord && (anyway || way.has(cord.id)) )
-						{
-							Object.values(cord).forEach((child)=>
-							{
-								if (!visited.has(child.ply.knot))
-								{
-									queue.push(child.trail(knot))
-								}
-							})
-						}	
-					})
-				}
-			}
-		}		
-	}
-	return {success:false,start:false,end:false,path:[]}  //not found
-}
-ishml.Knot.prototype.trail=function(knot)
-{
-	var newKnot=new ishml.Knot(this)
-	newKnot.ply.from=knot
-	newKnot.ply.hop=knot.ply.hop+1
-	return newKnot
-}
+
+
 
 //knot.twist1=storyline.twist(subject,{twist}) returns proxied subject.
 //knot.twist1.prop=7  creates a proxied property see on change libary.
