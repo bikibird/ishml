647fb92dd0932840ac9bd7c60e803724258e1791
dcsan <dc@rikai.co>
2020-07-08 03:00:18 -0700

    add key on output

--- server/story.js
+++ server/story.js
@@ -6,22 +6,24 @@ const logit = (msg, obj) => {
 }
 
 const inputs = [
-  "take the ruby slipper",
+  // "take the ruby slipper",
   // "drop the ring in the tumbler",
   // "put on the ring",
   // "take the glass",
   // "take the tumbler",
   // "take the glass slipper",
+  // "Take the really pretty ruby slipper",
+  "take the slipper from the woman and give it to the man"
 ]
 
 function main () {
   inputs.map(input => {
     const output = parser.analyze(input)
     const int = output.interpretations[0].gist
-    console.log('---\ninput:\t', input)
-    logit('verb', int.verb)
-    logit('adj', int.nounPhrase.adjectives ? int.nounPhrase.adjectives[0] : '') // just first one
-    logit('noun', int.nounPhrase.noun)
+    console.log('---\ninput:\t\t', input)
+    logit('verb/key', int.verb)
+    logit('adj/key', int.nounPhrase.adjectives ? int.nounPhrase.adjectives[0] : '') // just first one
+    logit('noun/key', int.nounPhrase.noun)
     console.log('=>', JSON.stringify(output, null, 2))
   })
 }
