790593838306e0cb32846fa5c5a31d6976547117
Jennifer L Schmidt <jenny@bikibird.com>
2020-07-30 18:10:52 -0500

    Fixed bug in separator  handling of empty string

--- src/lexicon.js
+++ src/lexicon.js
@@ -54,7 +54,7 @@ ishml.Lexicon.prototype.search = function (searchText, {regex=false,separator=/^
 				var discard=result.remainder.match(separator)
 				if (discard !== null)
 				{
-					result.remainder=result.remainder.slice(discard.length)
+					if (discard !==""){result.remainder=result.remainder.slice(discard.length)}
 					_results.unshift(result)
 				}
 			}
@@ -102,7 +102,7 @@ ishml.Lexicon.prototype.search = function (searchText, {regex=false,separator=/^
 								var discard=result.remainder.match(separator)
 								if (discard !== null)
 								{
-									result.remainder=result.remainder.slice(discard[0].length)
+									if (discard !==""){result.remainder=result.remainder.slice(discard[0].length)}
 									_results.unshift(result)
 								}
 							}
