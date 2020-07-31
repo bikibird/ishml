c34348ae28994646cdf176d3d0fc8585f7e22954
Jennifer L Schmidt <jenny@bikibird.com>
2020-07-31 13:02:19 -0500

    Fixed bug in parser

--- src/ishml.js
+++ src/ishml.js
@@ -271,7 +271,7 @@ ishml.Parser.prototype.analyze=function(text)
 	})
 	if (completeInterpretations.length>0)
 	{	var validInterpretations=completeInterpretations.filter(interpretation=>interpretation.gist.valid===true)
-		if(validInterpretations) {return {success:true, interpretations:validInterpretations}}
+		if(validInterpretations.length>0) {return {success:true, interpretations:validInterpretations}}
 		else {return {success:true, interpretations:completeInterpretations}}
 	}
 	else
