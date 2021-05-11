118b0ff8a6e699a5d4012e39fd15853cb1d8e505
bikibird <jenny@bikibird.com>
2021-05-11 09:39:30 -0500

    Updated

--- examples/cloakofdarkness.js
+++ examples/cloakofdarkness.js
@@ -1,93 +1,90 @@
+/*places*/	
+$
+	.tie("place").to("foyer")
+	.tie("place").to("cloakroom")
+	.tie("place").to("bar")
 
-plot.world.unfold =function()
-{
-	/*places*/	
-	$
-		.tie("place").to("foyer")
-		.tie("place").to("cloakroom")
-		.tie("place").to("bar")
-
-	$.place.foyer
-		.tie("west").to($.place.cloakroom)
-		.tie("south").to($.place.bar)
-		.knot.configure({
-			name:"Foyer of the Opera House",
-			description:`You are standing in a spacious hall, splendidly decorated in red and gold, with glittering chandeliers overhead. The entrance from the street is to the north, and there are doorways south and west.`
-		})
-	lexicon.register("foyer").as({part:"noun", number:ishml.enum.number.singular, select:()=>$.place.foyer})
-
-	$.place.cloakroom.knot.configure({
-		name:"Cloakroom",
-		description:`The walls of this small room were clearly once lined with hooks, though now only one remains.
-			The exit is a door to the east.`
+$.place.foyer
+	.tie("west").to($.place.cloakroom)
+	.tie("south").to($.place.bar)
+	.knot.configure({
+		name:"Foyer of the Opera House",
+		description:`You are standing in a spacious hall, splendidly decorated in red and gold, with glittering chandeliers overhead. The entrance from the street is to the north, and there are doorways south and west.`
 	})
-	lexicon.register("cloakroom").as({part:"noun", number:ishml.enum.number.singular, select:()=>$.place.cloakroom})
+lexicon.register("foyer").as({part:"noun", number:ishml.enum.number.singular, select:()=>$.place.foyer})
 
-	$.place.bar.knot.configure({
-		name:"Bar",
-		description:`The bar, much rougher than you'd have guessed after the opulence of the foyer to the north, is completely empty. There seems to be some sort of message scrawled in the sawdust on the floor.`
-	}) 
-	lexicon.register("bar").as({part:"noun", number:ishml.enum.number.singular, select:()=>$.place.bar})
+$.place.cloakroom.knot.configure({
+	name:"Cloakroom",
+	description:`The walls of this small room were clearly once lined with hooks, though now only one remains.
+		The exit is a door to the east.`
+})
+lexicon.register("cloakroom").as({part:"noun", number:ishml.enum.number.singular, select:()=>$.place.cloakroom})
 
-	/*things*/
+$.place.bar.knot.configure({
+	name:"Bar",
+	description:`The bar, much rougher than you'd have guessed after the opulence of the foyer to the north, is completely empty. There seems to be some sort of message scrawled in the sawdust on the floor.`
+}) 
+lexicon.register("bar").as({part:"noun", number:ishml.enum.number.singular, select:()=>$.place.bar})
 
-	$
-		.tie("fixture","small@is","brass@is").to("hook")
-		.tie(cords.thing,"wearable","dark@is","black@is","velvet@is").to("cloak")
-		.tie("thing","wearable","black@is","left@is").to("left_shoe")
-		.tie("thing","wearable","black@is","right@is").to("right_shoe")
-		.tie("thing").to("cup")
-		.tie("thing").to("saucer")
-		.tie("thing").to("table")
+/*things*/
 
-	$.thing.cloak.configure({
-		name:"cloak",
-		description:"the blackest black velvet."
-	})
-	lexicon.register("cloak").as({part:"noun", number:ishml.enum.number.singular, select:()=>$.thing.cloak})
+$
+	.tie("fixture","small@is","brass@is").to("hook")
+	.tie(cords.thing,"wearable","dark@is","black@is","velvet@is").to("cloak")
+	.tie("thing","wearable","black@is","left@is").to("left_shoe")
+	.tie("thing","wearable","black@is","right@is").to("right_shoe")
+	.tie("thing").to("cup")
+	.tie("thing").to("saucer")
+	.tie("thing").to("table")
+
+$.thing.cloak.configure({
+	name:"cloak",
+	description:"the blackest black velvet."
+})
+lexicon.register("cloak").as({part:"noun", number:ishml.enum.number.singular, select:()=>$.thing.cloak})
 
 
 
 
-	/*Actors*/
-	$
-		.tie("actor").to("player","jane")
+/*Actors*/
+$
+	.tie("actor").to("player","jane")
 
-	$.actor.player
-		.configure(ishml.lang.pronouns.epicene)
-	lexicon.register("player").as({part:"noun", number:ishml.enum.number.singular, select:()=>$.actor.player, role:"player"})
+$.actor.player
+	.configure(ishml.lang.pronouns.epicene)
+lexicon.register("player").as({part:"noun", number:ishml.enum.number.singular, select:()=>$.actor.player, role:"player"})
 
-	$.actor.player
-		.configure(ishml.lang.pronouns.epicene)
-	lexicon.register("player").as({part:"noun", number:ishml.enum.number.singular, select:()=>$.actor.player, role:"player"})
-		
-
+$.actor.player
+	.configure(ishml.lang.pronouns.epicene)
+lexicon.register("player").as({part:"noun", number:ishml.enum.number.singular, select:()=>$.actor.player, role:"player"})
 	
-	/*Actions*/
-	$
-		.tie("action").to("dropping","taking","inventorying")
-		
-
-	/*staging*/
-
-	$.actor.player
-		.tie("in").to($.place.foyer)
-		.tie("has skill").to($.action.dropping,$.action.inventorying,$.action.taking)
-		.tie("wears").to($.thing.cloak)
-		.tie("wears").to($.thing.left_shoe)
-		.tie("wears").to($.thing.right_shoe)
 
 
-	$.actor.jane
-		.tie("has skill").to($.action.dropping)
+/*Actions*/
+$
+	.tie("action").to("dropping","taking","inventorying")
+	
 
-	$.fixture.hook.tie("in").to($.place.cloakroom)
-	$.thing.saucer.tie("on").to($.thing.table)
-	$.thing.cup.tie("on").to($.thing.saucer)
+/*staging*/
+
+$.actor.player
+	.tie("in").to($.place.foyer)
+	.tie("has skill").to($.action.dropping,$.action.inventorying,$.action.taking)
+	.tie("wears").to($.thing.cloak)
+	.tie("wears").to($.thing.left_shoe)
+	.tie("wears").to($.thing.right_shoe)
+
+
+$.actor.jane
+	.tie("has skill").to($.action.dropping)
+
+$.fixture.hook.tie("in").to($.place.cloakroom)
+$.thing.saucer.tie("on").to($.thing.table)
+$.thing.cup.tie("on").to($.thing.saucer)
 
 	/*scenes-- like inform 7*/
 	//tbd
-}
+
 /*language*/
 /*lexicon
 	.register("hook","peg").as({part:"noun", number:ishml.enum.number.singular, select:()=>$.fixture.hook})
@@ -111,6 +108,5 @@ plot.world.unfold =function()
 		.as({plot:plot.action.hanging, part: "verb", particle:"up", preposition:"on"})	  	
 */
 
-story.plot.world.unfold()
 story.harken()
 
